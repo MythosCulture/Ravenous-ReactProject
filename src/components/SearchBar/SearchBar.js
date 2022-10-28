29958521c79ed0d4420b2087e0104414d7624be0
MythosCulture <sylvesternegron@gmail.com>
2022-10-28 13:53:00 -0500

    Set state of sorting options, added visual feedback

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-  'Best Match': 'best_match',
-  'Highest Rated': 'rating',
-  'Most Reviewed': 'review_count'
-};
+function SearchBar(props) {
 
-function renderSortByOptions() {
-  return Object.keys(sortByOptions).map(sortByOption => {
-    const sortByOptionValue = sortByOptions[sortByOption];
-    return <li key={sortByOptionValue}>{sortByOption}</li>;
-  });
-}
+  const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+  };
 
-function SearchBar() {
+  const [term, setTerm] = useState('');
+  const [location, setLocation] = useState('');
+  const [sortBy, setSortBy] = useState('best_match');
+
+  function handleSortByChange(sortByOption) {
+    setSortBy(sortByOption);
+  }
+
+  function handleTermChange(e) {
+    const term = e.target.value;
+    setTerm(term);
+  }
+
+  function handleLocationChange(e) {
+    const location = e.target.value;
+    setLocation(location);
+  }
+
+  function handleSearch(e) {
+    props.searchYelp(term, location, sortBy);
+    e.preventDefault(); //because 'let's go' is a link 
+  }
+
+  function getSortByClass(sortByOption) {
+    if(sortBy === sortByOption) {
+      return 'active';
+    }
+    return '';
+  }
+
+  function renderSortByOptions() {
+    return Object.keys(sortByOptions).map(sortByOption => {
+      const sortByOptionValue = sortByOptions[sortByOption];
+      return (
+        <li 
+          key={sortByOptionValue}
+          onClick={() => {handleSortByChange(sortByOptionValue)}}
+          className={getSortByClass(sortByOptionValue)}>
+            {sortByOption}
+        </li>
+      );
+    });
+  }
+
   return (
     <div className="SearchBar">
       <div className="SearchBar-sort-options">
@@ -23,15 +61,14 @@ function SearchBar() {
         </ul>
       </div>
       <div className="SearchBar-fields">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
+        <input placeholder="Search Businesses" onChange={handleTermChange} />
+        <input placeholder="Where?" onChange={handleLocationChange} />
       </div>
       <div className="SearchBar-submit">
-        <a>Let's Go</a>
+        <a onClick={handleSearch}>Let's Go</a>
       </div>
     </div>
   );
-
 }
 
 export default SearchBar;
